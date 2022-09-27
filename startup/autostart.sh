7c37168ef708388c2c1209672ca7a22c54c9260a
jalupa <jalupa@jalupa.fake>
2022-09-27 13:59:31 +0200

    removed bluetooth from autostart, added it to dmenuscripts/fix

--- startup/autostart.sh
+++ startup/autostart.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-picom --experimental-backends &
 dunst &
 clipmenud &
-emacs --daemon
+emacs --daemon &
 feh --bg-fill $HOME/Pictures/Backgrounds/currentBackground.jpg
 $HOME/.config/jalupa_config/startup/dwmBar.sh &
-mpris-proxy & #allow bluetooth media control keys
+picom --experimental-backends &
+# mpris-proxy & #allow bluetooth media control keys
 
-sleep 3
-/bin/sh -c "pulseaudio --daemonize" #fix bluetooth connection
+# sleep 3
+# /bin/sh -c "pulseaudio --daemonize" #fix bluetooth connection
