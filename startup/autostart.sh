f0b3c22f1ea881016b8bc07502588035d7da3d33
jalupa <jalupa@jalupa.fake>
2022-12-04 16:13:42 +0100

    added https://snowflake.torproject.org to start on startup

--- startup/autostart.sh
+++ startup/autostart.sh
@@ -6,6 +6,7 @@ feh --bg-fill $HOME/Pictures/Backgrounds/currentBackground.jpg
 $HOME/.config/jalupa_config/startup/dwmBar.sh &
 picom --experimental-backends &
 syncthing --no-browser &
+nohup /home/jakob/repos/own/snowflake/proxy/proxy /home/jakob/.snowflake.log 2>&1 &
 # mpris-proxy & #allow bluetooth media control keys
 
 # sleep 3
