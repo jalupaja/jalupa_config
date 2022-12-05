2d32ee838c3bc05c92e9236d47f74d239df3f8fb
jalupa <jalupa@jalupa.fake>
2022-12-05 11:11:09 +0100

    changed emacs comment color

--- emacs/config.el
+++ emacs/config.el
@@ -33,6 +33,8 @@
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
 (setq doom-theme 'doom-ayu-dark)
+;; (set-face-foreground 'font-lock-string-face "dark green")
+(set-face-foreground 'font-lock-comment-face "dark green")
 ;; (setq doom-theme 'leuven) ; bright theme
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
@@ -41,7 +43,7 @@
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/Documents/org/")
+(setq org-directory "~/Documents/lists/")
 
 
 ;; Whenever you reconfigure a package, make sure to wrap your config in an
@@ -77,6 +79,7 @@
 ;; they are implemented.
 
 ;; Hotkeys
+; 
 ; code
 (map! :leader
       :desc "Comment code"
